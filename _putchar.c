661a281ce5e425bdef5de0367a0b90248ffca8b2
Veeviyan <snowygeenie@gmail.com>
2023-04-23 13:09:03 -0700

    created _putchar.c and main.h files

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+  * _putchar - prints a character to stdout
+  * @c: character to be printed
+  * Return: value of character
+  */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
