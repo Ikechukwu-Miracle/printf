ede96016756f39817c4548a7c5b5a4f5eb85c296
Ikechukwu_Miracle_Nweke <nwekeiyke46@gmail.com>
2023-04-24 10:28:35 -0700

    editted _printf.c

--- _printf.c
+++ _printf.c
@@ -11,36 +11,33 @@ int _printf(const char *format, ...)
 	int i, val, r_val = 0;
 	va_list print;
 
+	va_start(print, format);
+
 	if (format == NULL)
 		return (-1);
-	else
-	{
-		va_start(print, format);
 
-		for (i = 0 ; format[i] ; i++)
+	for (i = 0 ; format[i] ; i++)
+	{
+		if (format[i] != '%')
+			_putchar(format[i]);
+		else if (format[i + 1] == 'c')
 		{
-			if (format[i] != '%')
-			{
-				_putchar(format[i]);
-			}
-			else if (format[i + 1] == 'c')
-			{
-				_putchar(va_arg(print, int));
-				i++;
-			}
-			else if (format[i + 1] == '%')
-			{
-				_putchar('%');
-				i++;
-			}
-			else if (format[i + 1] == 's')
-			{
-				val = put_string(va_arg(print, char *));
-				i++;
-				r_val = val - 1;
-			}
-			r_val += 1;
+			_putchar(va_arg(print, int));
+			i++;
 		}
+		else if (format[i + 1] == '%')
+		{
+			_putchar('%');
+			i++;
+		}
+		else if (format[i + 1] == 's')
+		{
+			val = put_string(va_arg(print, char *));
+			i++;
+			r_val = val - 1;
+		}
+
+		r_val += 1;
 	}
 	return (r_val);
 }
